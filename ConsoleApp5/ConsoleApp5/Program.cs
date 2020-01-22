73c5c45a1ad38cdf1591bab41638d21d92b66964
nmansi97 <50015433+nmansi97@users.noreply.github.com>
2020-01-22 10:27:33 +0530

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    interface rate
+    {
+        
+        int getdays();
+        double getkm();
+    }
+    abstract class car : rate
+    {
+      public  int carno;
+       public  string name;
+        abstract public void getcarinfo();
+        public abstract int getdays();
+        public abstract double getkm();
+    }
+    
+
+    class customer : car
+    {
+        
+        public override void getcarinfo()
+        {
+          
+            Console.WriteLine("enter carno=");
+            int carno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter carname=");
+            string name = Console.ReadLine();
+
+        }
+        public override int getdays()
+        {
+            Console.WriteLine("enter days=");
+            int days = Convert.ToInt32(Console.ReadLine());
+            return days;
+        }
+        public override double getkm()
+        {
+            Console.WriteLine("enter kms=");
+            double km = Convert.ToDouble(Console.ReadLine());
+            return km;
+        }
+    
+        public void calc_amt()
+        {
+            int d = getdays();
+            double k = getkm();
+            double rate_km = 7.5;
+            int rate_Day = 500;
+            double amt_kms = rate_km * k;
+            double amt_days = rate_Day * d;
+            Console.WriteLine("total kms="+ k);
+            Console.WriteLine("total days="+d);
+            Console.WriteLine("total amount based on kms=" + amt_kms);
+            Console.WriteLine("total amount based on days=" + amt_days);
+        }
+       
+        
+       
+    }
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            customer c1 = new customer();
+            c1.getcarinfo();
+           
+         
+           
+            c1.calc_amt();
+
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        public static void calltochildthread()
+        {
+            try
+            {
+                Console.WriteLine("child thread starts...");
+                for (int c = 0; c <= 10; c++)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine(c);
+                }
+                Console.WriteLine("child thread completed...");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("thread abort exception");
+            }
+        }
+        public static void calltochildthread2()
+        {
+            try
+            {
+                Console.WriteLine("child thread starts...");
+                for (char i = 'a'; i <= 'j'; i++)
+                {
+                    Thread.Sleep(100);
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("child thread completed...");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("thread abort exception");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadStart child1 = new ThreadStart(calltochildthread);
+            Console.WriteLine("creaing the child thread:");
+            Thread childthread = new Thread(child1);
+            childthread.Start();
+          
+            ThreadStart child2 = new ThreadStart(calltochildthread2);
+            Console.WriteLine("creaing the child thread:");
+            Thread childthread2 = new Thread(child2);
+            childthread2.Start();
+            Thread.Sleep(1000);
+            Thread.Sleep(2000);
+            Console.WriteLine("In Main: Aborting the Child thread");
+            childthread.Abort();
+
+
+        }
+    }
+}
